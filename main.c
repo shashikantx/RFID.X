4c6374c7dd9a028324f2d4d0c8ee3f3f95047bf0
shashikantx <shashikantrudrawadi@gmail.com>
2019-04-04 23:06:47 +0530

    add lock and functional calls

--- main.c
+++ main.c
@@ -46,6 +46,7 @@
 enum MIFARE_CARD_TYPE {
     UID_FAILED = 0x00,
     UID_NOT_COMPLETE = 0x04,
+    MIFARE_DESFIRE = 0x24, //MIFARE DESFire CL1, MIFARE DESFire EV1 CL1 
     MIFARE_MINI = 0x09,
     MIFARE_CLASSIC_1K = 0x08,
     MIFARE_CLASSIC_4K = 0x18,
@@ -70,9 +71,11 @@ char buffer2[20];
 byte keyA[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 byte keyB[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
+byte LockKey[6] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06};
+
 
 //writeable data 
-byte writeData[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10};
+byte writeableData[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10};
 
 
 
@@ -80,12 +83,13 @@ byte writeData[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 
 ///     Check RFID Hardware
 /////////
 
-void CheckRFIDHardware() {
+int1 CheckRFIDHardware() {
 
-    char version; ///Version of RFID read/writer is stored 0x92 is version 2.0
+    char version; 
+    ///Version of RFID read/writer is stored
+    // version 2.0 is 0x92
     ///Version 1.0 is 0x91
-
-    lcd_putc("\f Checking RFID...");
+    
     printf("\r\nChecking If Rfid Is Connected");
 
     //Request from the register
@@ -98,13 +102,17 @@ void CheckRFIDHardware() {
         lcd_putc("\f RFID Not Found \n Check Again..");
         printf("\r\nRFID Hw not Found");
         delay_ms(5000);
+        return 0;
     }
     if (version) {
         lcd_putc("\f RFID Found");
         printf(lcd_putc, "\nV: %x", version);
         printf("\r\nRFID Found and HW version is: 0x%2X\r\n", version);
         delay_ms(1000);
+        return 1;
     }
+    
+    return 0;
 
 }
 
@@ -114,14 +122,14 @@ void CheckRFIDHardware() {
 ///     Read all the data from the mifare card and out puts to serial terminal    
 /////
 
-void readMifareCard(void) {
+void readAll(void) {
 
     //s50 has 64 blocks of memory
     for (i = 0; i < 64; i++) {
         // Try to authenticate each block first with the A key.
         status = authenticate(MF1_AUTHENT1A, i, keyA, serial);
         if (MI_OK == status) {
-            // printf("Authenticated block 0x%2X with A\r\n",i);
+            printf("Authenticated block 0x%2X with A\r\n",i);
             printf("\r\n"); //a new line each block
             //now read the block i 
             status = readFromTag(i, data);
@@ -138,7 +146,7 @@ void readMifareCard(void) {
             //try to authenticate with key B
             status = authenticate(MF1_AUTHENT1B, i, keyB, serial);
             if (MI_OK == status) {
-                //printf("Authenticated block 0x%2X with key B",i);
+                printf("Authenticated block 0x%2X with key B",i);
                 printf("\r\n"); //a new line each block
 
                 //read from block
@@ -162,42 +170,186 @@ void readMifareCard(void) {
 
 }
 
-void writeMifare(void){
-    int block = 4;
 
-                    status = authenticate(MF1_AUTHENT1A, 2, keyA, serial);
-
-                    //authenticated with key A
-                    if (MI_OK == status) {
-                        printf("Authenticated with key A \r\n");
-                        status = writeToTag(2, writeData);
-                        if (MI_OK == status) {
-                            printf("Write Successful\r\n");
-                        }else{
-                            printf("Write failed\r\n");
-                        }
+    // mifare classic has 16 sectors 
+    // and 4 blocks each sector
+    // Sectors 0 to 15
+    // Blocks 0 to 3
+    // Block 3  is trailer block
+    // Block 0 from Sector 0 manufacturer block, non editable
+    
 
 
-                    } else {
-                        //try to authenticate with key b
-                        status = authenticate(MF1_AUTHENT1B, 2, keyB, serial);
-                        if (MI_OK == status) {
-                            printf("Authenticated with key B \r\n");
-                            status = writeToTag(2, writeData);
-                            if (MI_OK == status) {
-                                printf("Write Successful\r\n");
-                            }else{
-                                printf("Write Failed");
-                            }
+void readBlock(int sector, int block, byte* readData) {    
+    
+    
+    //calculate block number for given sector and 
+    // corresponding block number
+    int readBlock = ( sector * 4 ) + block;    
 
-                        } else {
-                            printf("Couldn't access block 0x%X write failed", 2);
-                        }
+    // Try to authenticate each block first with the A key.
+    status = authenticate(MF1_AUTHENT1A, readBlock, keyA, serial);
+    if (MI_OK == status) {
+        printf("Authenticated block 0x%2X with Key A\r\n",i);
+        printf("\r\n"); //a new line each block
+        //now read the block i 
+        status = readFromTag(readBlock, data);
+        if (MI_OK == status) {
+            for (j = 0; j < 15; j++) {                
+                readData[j] = data[j];
+            }
+            readData[15] = data[15];
+        } else {
+            printf("Read failed\r\n");
+        }
 
-                    }
+    } else {
+        //try to authenticate with key B
+        status = authenticate(MF1_AUTHENT1B, readBlock, keyB, serial);
+        if (MI_OK == status) {
+            printf("Authenticated block 0x%2X with key B",i);
+            printf("\r\n"); //a new line each block
 
+            //read from block
+            status = readFromTag(i, data);
 
+            if (MI_OK == status) {
+                for (j = 0; j < 15; j++) {                
+                    readData[j] = data[j];
+                }
+                readData[15] = data[15];
+            } else {
+                printf("Read failed \r\n");
+            }
+
+        } else {
+            printf("Access denied at block 0x%2X", i);
+        }
+    }
 }
+
+void writeBlock(int sector, int block, byte* writeData){  
+    
+    //calculate block number for given sector and 
+    // corresponding block number
+    int writeBlock = ( sector * 4 ) + block;
+
+    status = authenticate(MF1_AUTHENT1A, writeBlock, keyA, serial);
+
+    //authenticated with key A
+    if (MI_OK == status) {
+        printf("Authenticated with key A \r\n");
+        status = writeToTag(writeBlock, writeData);
+        if (MI_OK == status) {
+            printf("Write Successful\r\n");
+        }else{
+            printf("Write failed\r\n");
+        }
+
+
+    } else {
+        //try to authenticate with key b
+        status = authenticate(MF1_AUTHENT1B, writeBlock, keyB, serial);
+        if (MI_OK == status) {
+            printf("Authenticated with key B \r\n");
+            status = writeToTag(writeBlock, writeData);
+            if (MI_OK == status) {
+                printf("Write Successful\r\n");
+            }else{
+                printf("Write Failed");
+            }
+
+        } else {
+            printf("Couldn't access block 0x%X write failed", 2);
+        }
+
+    }
+}
+
+void writeLockedBlock(int sector, int block, byte* writeData, byte* lockKey){  
+    
+    int writeBlock = ( sector * 4 ) + block;
+
+    status = authenticate(MF1_AUTHENT1B, writeBlock, lockKey, serial);
+
+    //authenticated with key A
+    if (MI_OK == status) {
+        printf("Authenticated with Lockkey \r\n");
+        status = writeToTag(writeBlock, writeData);
+        if (MI_OK == status) {
+            printf("Write Successful\r\n");
+        }else{
+            printf("Write failed\r\n");
+        }
+
+
+    } else{
+        printf("\nWrite failed, wrong key supplied \r\n");
+    }
+}
+
+
+
+void readLockedBlock(int sector, int block, byte* readData, byte* lockKey) {    
+    
+    
+    //calculate block number for given sector and 
+    // corresponding block number
+    int readBlock = ( sector * 4 ) + block;    
+
+    // Try to authenticate each block first with the A key.
+    status = authenticate(MF1_AUTHENT1B, readBlock, lockKey, serial);
+    if (MI_OK == status) {
+        printf("Authenticated block 0x%2X with LockKey \r\n",i);
+        printf("\r\n"); //a new line each block
+        //now read the block i 
+        status = readFromTag(readBlock, data);
+        if (MI_OK == status) {
+            for (j = 0; j < 15; j++) {                
+                readData[j] = data[j];
+            }
+            readData[15] = data[15];
+        } else {
+            printf("Read failed\r\n");
+        }
+
+    } else {       
+        printf("\nRead failed, wrong key supplied \r\n");
+    }
+}
+
+
+//lock a sector with write key
+//you can read only with readKey
+//and you can write with writeKey
+
+void LockSector(int sector, byte* readKey, byte* writeKey){
+    
+    byte accessBit[MAX_LEN];
+    
+    for(int i = 0; i< 6; i++){
+      accessBit[i]  = readKey[i];
+    }    
+    
+    accessBit[6] = 0x78;
+    
+    accessBit[7] = 0x77;
+    
+    accessBit[8] = 0x88;
+    
+    accessBit[9] = 0x69;
+    
+    for(int j = 0; j < 6; j++){
+        accessBit[10+j] = writeKey[j];
+    }
+    
+    for(int a = 0; a < 16; a++){
+        printf(" %2X",accessBit[a]);
+    }
+    
+    writeBlock(sector,3, accessBit);
+}
+
 
 void main() {
 
@@ -217,27 +369,36 @@ void main() {
     //initialize RFID Reader
 
     delay_ms(100);
+    
     MFRC_begin();
 
     //check if the card reader is connected.
-    CheckRFIDHardware();
+    while(CheckRFIDHardware() == 0){
+        
+        delay_ms(100);
+    
+        MFRC_begin();
+    }
+    //keep looping trying to find hardware
 
-    /////  
-
-
+    //in while loop keep checking for any new card
     while (TRUE) {
+        
+        printf("\r\nSearching for card");
+        
+        lcd_putc("\f Searching for \nrfid...");
 
-        //        
         //      //  Check to see if a Tag was detected
         //     // If yes, then the variable FoundTag will contain "MI_OK"
         FoundTag = requestTag(MF1_REQIDL, TagData);
-
         //
-        //If found tag
+        //If tag found
         if (FoundTag == MI_OK) {
 
-            printf("\r\nFound Tag: ");
-
+            printf("\r\nFound Tag: ");            
+            lcd_putc("\r\nFound Tag: ");
+            
+            //when multiple cards in horizon choose one.
             ReadTag = antiCollision(TagData);
 
             ///has tag passed anti collision test?
@@ -250,10 +411,10 @@ void main() {
                     printf(buffer2);
                     printf(", ");
                 }
-
                 sprintf(buffer2, "%2X", (TagData[i])); //display version in hexadecimal
                 printf(buffer2);
-
+                
+                //copy tag serial number to `serial` variable
                 memcpy(serial, TagData, 5);
 
                 //now get SAK for card version check
@@ -268,23 +429,53 @@ void main() {
                 else if (UID_NOT_COMPLETE == sak) {
                     printf("\r\n Incomplete UID");
                 } else {
-                    printf("\r\n Is a Mifare Card");
-
+                    printf("\r\n Is a Mifare Card\n");
                     //everything went well so far                        
                     //now start reading memory
-                    readMifareCard();
-                    //writeMifare();
-                   
-
-                    haltTag();
                     
-
+//                  //if you wan to read whole card
+                    //readAll();
+                    
+                    //MAX_LINE is 16
+                    byte readData[MAX_LEN];                    
+                    
+                    //if you want to read from
+                    // sector 6
+                    // block 2
+                    // data into readData array of size 16 bytes
+                    
+                    //read a block with keyA or keyB                    
+                    readBlock( 6, 2, readData );                    
+                    
+                    readLockedBlock(6,3,readData,LockKey);
+                    
+//                    
+//                   for(int x=0;x<MAX_LEN;x++){
+//                       printf("%2X ",readData[x]);
+//                    }
+                    
+                    
+                    
+                    //if you want to write something to card
+                    byte writedata[] = {0x0F, 0x0F, 0x0F, 0x0F, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10};
+                    
+                    writeBlock(6, 2, writedata);
+                    
+                    //if you want to write to a block in locked sector
+                    writeLockedBlock(6, 2, writedata, LockKey);
+                    
+                    
+                    //if you want to lock a sector with new key
+                    // keyA is a key you want to use to read and can be public
+                    //lockKey is key you want lock sector with, it should be private
+                    //we can modify locked content with LockKey
+                    LockSector(6,keyA,LockKey);                    
+                    
+                    
+                    //always halt tag once the card is processed
+                    haltTag();
                 }
-
-
             }
-
-
         }
 
         delay_ms(2000);
